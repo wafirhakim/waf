5d12a87cf19a9a34b71d19eb0ee5ad8e7acce8ce
Waf Wahid <wwahid@hubspot.com>
2022-11-09 17:55:12 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.get("/", function(req, res){
+    res.render('home');
+  });
+app.listen(3000, function()
+  {console.log("Server started on port 3000");}
+);
+app.set("view engine", "ejs");
